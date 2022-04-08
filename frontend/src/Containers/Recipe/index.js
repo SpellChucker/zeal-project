eddc0f37a45e5c2d379dd408f5cfa035e91dca9f
Todd Vrba <todd.vrba@gmail.com>
2022-04-07 21:21:29 -0500

    Add in recipe component pulling recipe from store

--- frontend/src/Containers/Recipe/index.js
+++ frontend/src/Containers/Recipe/index.js
@@ -1,1 +1,30 @@
-// TODO Create a connected component to render a fetched recipe
+import React from "react"
+import { connect } from "react-redux"
+
+// Create a recipe component using the recipe reducer to display the recipe.
+export const Recipe = ({ recipe }) => {
+  if (!recipe) {
+    return null
+  }
+
+  return (
+    <div>
+      <h1>{recipe.name}</h1>
+      <p>{recipe.instructions}</p>
+      <ul>
+        {recipe.ingredients.map((ingredient) => {
+          const { _id, name, amount, unit } = ingredient
+
+          return <li key={_id}>{amount} {unit} {name}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  const { recipe } = state
+  return { ...recipe }
+}
+
+export default connect(mapStateToProps)(Recipe)
