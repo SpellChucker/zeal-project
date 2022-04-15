c42dbce0c70520cecb479dd711a3f57ae2324396
Todd Vrba <todd.vrba@gmail.com>
2022-04-14 20:33:04 -0500

    Fix logic in search endpoint

--- backend/src/routes/recipe.ts
+++ backend/src/routes/recipe.ts
@@ -1,11 +1,11 @@
-import express, { NextFunction, Request, Response } from "express"
+import express, { Request, Response } from "express"
 import { NotFoundError } from "../errors"
 import { RecipeModel } from "../models"
 import 'express-async-errors'
 
 const router = express.Router()
 
-router.get("/api/recipe/:id", async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+router.get("/api/recipe/:id", async (req: Request, res: Response): Promise<void> => {
   const recipe = await RecipeModel.findById(req.params.id)
 
   if (!recipe) {
--- backend/src/routes/search.ts
+++ backend/src/routes/search.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const allIngredients = ["flour", "sugar", "salt", "butter", "milk"]
 
-const escapeRegex = (text): string => {
+const escapeRegex = (text: string): string => {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
 }
 
@@ -25,13 +25,18 @@ router.post("/api/search", async (req: Request, res: Response): Promise<void> =>
   if (name) {
     query.name = new RegExp(escapeRegex(name), "gi")
   }
-  if (ingredients) {
+  if (ingredients && ingredients.length > 0) {
     const whatsLeft = allIngredients.filter((ing) => !ingredients.includes(ing))
     query["ingredients.name"] = { $nin: whatsLeft }
+
+    const foundRecipes = await RecipeModel.find(query)
+    const builtRecipes = foundRecipes.map(recipeCleaner)
+
+    res.send(builtRecipes)
+  } else {
+    // No ingredients passed, so send no recipes.
+    res.send([])
   }
-  const foundRecipes = await RecipeModel.find(query)
-  const builtRecipes = foundRecipes.map(recipeCleaner)
-  res.send(builtRecipes)
 })
 
 export { router as searchRouter }
